c34987663adb69d1ac03301f8226d3c78967e5b1
ligeng910118 <31567614+ligeng910118@users.noreply.github.com>
2018-05-22 17:45:15 +0800

    5-22 17:45

--- js/model.js
+++ js/model.js
@@ -12,12 +12,32 @@ var singleton = function()
 	}
 	
 	return {
-		publicProperty:true;
+		publicProperty:true,
 	
 		publicMethod:function()
 		{
 			privateVaribale++;
 			return privateFunction();
 		}
+	};
+}();
+
+var singletonTwo = function()
+{
+	var privateVaribale = 10;
+	
+	function privateFunction()
+	{
+		return false;
 	}
-}
+	
+	var obj = {};
+	obj.publicProperty = true;
+	obj.publicMethod = function()
+	{
+		privateVaribale++;
+		return privateFunction();
+	};
+	
+	return obj;
+}();
