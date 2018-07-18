f1754c7497b74b5852e484fb4e46d276cec1f181
ligeng910118 <2316558806@qq.com>
2018-07-18 19:08:12 +0800

    7-18 19:08

--- c#/IApi.cs
+++ c#/IApi.cs
@@ -66,5 +66,36 @@ class Publication : IListable
 	public string Author { get;set; }
 	public int Year { get;set; }
 
-	
+	public Publication(string title,string author,int year)
+	{
+		Title = title;
+		Author = author;
+		Year = year;
+	}
+
+	public string[] ColumnValues
+	{
+		get
+		{
+			return new string[]
+			{
+				Title,
+				Author,
+				Year.ToString()
+			};
+		}
+	}
+2
+	public static string[] Headers
+	{
+		get
+		{
+			return new string[]
+			{
+				"title",
+				"author",
+				"year"
+			}
+		}
+	}
 }
