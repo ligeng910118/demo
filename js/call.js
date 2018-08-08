cf3e8a8aabe9840fa78d5b9d2befc6df1fd63a7e
ligeng910118 <2316558806@qq.com>
2018-08-08 21:51:01 +0800

    Revert "Merge branch 'master' of https://github.com/ligeng910118/demo"
    This reverts commit fed2e72a628cf6c50c86e1bf7202f8e19f0e731c, reversing
    changes made to de60ab997b466fc154f524cc43ce09d7a04bc4bb.

--- js/call.js
+++
@@ -1,9 +0,0 @@
-function changeStyle(attr, value)
-{     
-    this.style[attr] = value; 
- }  
-var box = document.getElementById('box');  
-window.changeStyle.call(box, "height", "200px");
-
-
-//box.style.height=”200px”；
